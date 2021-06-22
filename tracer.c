a0c2c130bdfe66a316b607eee7094b8d173ae986
kostya <kish@campus.technion.ac.il>
2021-06-22 17:20:01 +0000

    implemented addrOfFunctionNamed

--- tracer.c
+++ tracer.c
@@ -4,6 +4,18 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/ptrace.h>
+#include <elf64.h>
+#include <fcntl.h>
+#include <string.h>
+
+#define true    1
+#define false   0
+#define LOCAL   0
+#define GLOBAL  1
+#define NOTYPE  0
+#define FUNC    2
+
+typedef unsigned char bool;
 
 struct user_regs_struct {
     unsigned long long int r15;
@@ -37,8 +49,60 @@ struct user_regs_struct {
 
 int spid;
 
-void* addrOfFunctionNamed(char* func_name, char* elf_name){
-    
+typedef enum {FOUND_GLOBAL, FOUND_LOCAL, NOT_FOUND}  funcBindResult;
+
+funcBindResult addrOfFunctionNamed(char* func_name, char* elf_name, Elf64_Addr* addr){
+    Elf64_Ehdr elf_header;
+    int fd = ropen(elf_name);                       //add do_sys
+    pread(fd, &elf_header, sizeof(Elf64_Ehdr), 0);  //add do_sys
+
+    Elf64_Xword sym_size;
+    Elf64_Xword sym_entsize;
+    uint64_t sym_num;
+    Elf64_Off sym_offset;
+    Elf64_Off elf_shoff = elf_header.e_shoff;
+    bool foundSym = false;
+    for(uint64_t i = 0; i < elf_header.e_shnum; ++i) {
+        Elf64_Word sh_name = elf_shoff; // unsigened 64 bit truncated to 32 is well defined
+        if (strcmp((char*) sh_name, ".symtab") == 0) { 
+                Elf64_Shdr shdr;
+                pread(fd, &shdr, sizeof(Elf64_Shdr), elf_shoff); //add do_sys
+                sym_size = shdr.sh_size;
+                sym_entsize = shdr.sh_entsize;
+                sym_offset = shdr.sh_offset;
+                sym_num = sym_size/sym_entsize; //assert(sym_size == sym_entsize*sym_num)
+                foundSym = true;
+        }
+        elf_shoff += elf_header.e_shentsize;
+    }
+
+    if (!foundSym) {
+        return;
+    }
+
+    for(uint64_t i = 0; i < sym_num; ++i) {
+        Elf64_Word sym_name = sym_offset;
+        if (strcmp((char*)sym_name, func_name) == 0) {
+            Elf64_Sym sym;
+            pread(fd, &sym, sizeof(Elf64_Sym), sym_offset); //add do_sys
+            unsigned char info_copy1 = sym.st_info;
+            unsigned char bind = ELF64_ST_BIND(info_copy1);
+            unsigned char info_copy2 = sym.st_info;
+            unsigned char type = ELF64_ST_TYPE(info_copy2);
+            if ((bind == GLOBAL) && ((type == NOTYPE) || (type == FUNC))) {//notype needed?
+                *addr = sym.st_value;
+                return FOUND_GLOBAL;
+            }
+            else if ((bind == LOCAL) && ((type == NOTYPE) || (type == FUNC))) {
+                *addr = NULL;
+                return FOUND_LOCAL;
+            }
+        }
+        sym_offset += sym_entsize;
+    }
+
+    *addr = NULL;
+    return NOT_FOUND;
 }
 
 int isInstrSyscall(void** instr_addr){
